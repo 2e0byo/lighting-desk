6216846ba90cc7a1813b1b026e6222de68bde37a
John Maximilian <2e0byo@gmail.com>
2021-12-26 22:21:07 +0100

    feat: add password

--- lighting-desk/src/components/channels.vue
+++ lighting-desk/src/components/channels.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-if="$root.wampIsOpen">
     <Channel
       v-for="(val, index) in vals"
       :key="index"
@@ -11,6 +11,16 @@
          :number="3"
          :val="7"
          /> -->
+    </div>
+    <div v-else>
+      <md-dialog-prompt
+      :md-active.sync="!$root.wampIsOpen"
+      v-model="password"
+      md-title="Enter Password"
+      md-input-placeholder="Password"
+      md-confirm-text="Login"
+      v-on:md-confirm="reconnect"
+    />
   </div>
 </template>
 
@@ -26,18 +36,29 @@ export default {
     return {
       controllerName: null,
       vals: [],
+      password: "",
     }
   },
   wamp: {
     subscribe: {
       "controller.statechange"(args) {
-        args[0].forEach((val, i) => this.$refs[`Channel${i}`][0].val = val)
+        args[0].forEach((val, i) => (this.$refs[`Channel${i}`][0].val = val))
       },
     },
   },
   async mounted() {
-    const info = await this.$wamp.call("details")
-    this.vals = info.vals
+    // It would probably be better to reconnect or move the connection logic here.
+    const self = this
+    this.$wamp._connection._options.onchallenge = function () {
+      return self.password
+    }
+  },
+  methods: {
+    reconnect: async function () {
+      this.$wamp._connection.open()
+      const info = await this.$wamp.call("details")
+      this.vals = info.vals
+    },
   },
 }
 </script>
